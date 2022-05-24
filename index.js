c2f5c212e38e9ca9a96789f82c879dfeed55238d
Shahrear-ahamed <dev.shahrear@gmail.com>
2022-05-24 09:07:32 +0600

    insert user login data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+var jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
@@ -30,10 +31,29 @@ const run = async () => {
 
     // database
     const productCollection = client.db("anchor-auto").collection("products");
+    const userCollection = client.db("anchor-auto").collection("users");
 
     app.get("/products", async (req, res) => {
       const result = await productCollection.find().toArray();
       res.send(result);
+    });
+    // make user collection
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      console.log(user);
+      const filter = { email };
+      const updatedData = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updatedData, {
+        upsert: true,
+      });
+      const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+        expiresIn: "1d",
+      });
+      res.send({ result, token });
     });
   } finally {
     // client.close()
