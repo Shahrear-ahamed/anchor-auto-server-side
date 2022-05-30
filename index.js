775b6c418e6b75a3bbb5eb62e1ea94616dc2133d
Shahrear-ahamed <dev.shahrear@gmail.com>
2022-05-30 22:20:53 +0600

    fix cors error

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ const app = express();
 
 // middleware are here
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 app.use(express.json());
 
 const verifyJwt = (req, res, next) => {
